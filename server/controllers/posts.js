57c07fa2ba45145b5c6dac51a93a354cf7656346
dimassabimanyy <dmsabmny@gmail.com>
2021-05-07 09:27:58 +0700

    add update post feature & customize form component

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import express from "express";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -18,4 +20,23 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+
+  // Check if the id is valid with mongoose
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id : ${id}`);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { ...post, id },
+    {
+      new: true,
+    }
+  );
+
+  res.json(updatedPost);
 };
