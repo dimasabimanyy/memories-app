d86113dd2415d8e81e5c33806d608893247e36ec
dimassabimanyy <dmsabmny@gmail.com>
2021-05-08 10:55:54 +0700

    add like functionality & dev config

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -51,3 +51,20 @@ export const deletePost = async (req, res) => {
   await PostMessage.findByIdAndRemove(id);
   res.json({ message: "Post deleted successfully" });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  // Check if the id is valid with mongoose
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id : ${id}`);
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+
+  res.json(updatedPost);
+};
