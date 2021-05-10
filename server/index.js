91b7eb53e2f7ebee0893a634fa4b430666ec7d2d
dimassabimanyy <dmsabmny@gmail.com>
2021-05-10 11:56:57 +0700

    add google login/logout feature

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import path from "path";
 import connectDB from "./config/db.js";
 import postsRoutes from "./routes/posts.js";
+import usersRoutes from "./routes/users.js";
 
 dotenv.config({ path: "./config/config.env" });
 const app = express();
@@ -15,6 +16,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/user", usersRoutes);
 app.use("/posts", postsRoutes);
 
 const PORT = process.env.PORT || 5000;
